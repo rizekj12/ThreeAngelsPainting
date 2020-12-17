c29e28f9fcb40db0ec569d6b03640a6dd84cafb5
Joshua Rizek <joshuarizek@gmail.com>
2020-12-16 19:45:29 -0500

    changed formatting of code to be more organized

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,18 +6,18 @@ const Nav = () => {
   return (
     <div className="headingDiv">
         <Link to="header" smooth={true} duration={1000}>
-        <h1 className="headingTitle">Three Angels Painting</h1>
+            <h1 className="headingTitle">Three Angels Painting</h1>
         </Link>
-      <div className="navLinks">
-        <Link className="quoteText linkText" to="quote" smooth={true} duration={1000}> Quote </Link>
-        <Link className="linkText" to="services" smooth={true} duration={1000}> Services </Link>
-        <Link className="linkText" to="our-work" smooth={true} duration={1000}>
-            <div className="ourWorkTexts">
-            <span className="ourWork">Our </span><span className="ourWork"> Work</span>
-            </div>
-        </Link>  
-        <Link className="linkText" to="testimonials" smooth={true} duration={1000}> Testimonials </Link>
-        <Link className="linkText" to="about-us" smooth={true} duration={1000}> About </Link>
+        <div className="navLinks">
+            <Link className="quoteText linkText" to="quote" smooth={true} duration={1000}> Quote </Link>
+            <Link className="linkText" to="services" smooth={true} duration={1000}> Services </Link>
+            <Link className="linkText" to="our-work" smooth={true} duration={1000}>
+                <div className="ourWorkTexts">
+                    <span className="ourWork">Our </span><span className="ourWork"> Work</span>
+                </div>
+            </Link>  
+            <Link className="linkText" to="testimonials" smooth={true} duration={1000}> Testimonials </Link>
+            <Link className="linkText" to="about-us" smooth={true} duration={1000}> About </Link>
       </div>
     </div>
   );
