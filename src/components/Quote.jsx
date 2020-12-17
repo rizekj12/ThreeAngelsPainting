475df911948e35010af4b4f711de3fc6c73e9dc0
Christian Matos <chris@Christians-MBP.fios-router.home>
2020-12-17 14:42:23 -0500

    footer&work

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import "../CSS/Quote.scss"
 
 const Quote = () => {
-    return ( 
-        <section id="quote">
-            <div>
-                <h1 className="title">REQUEST A QUOTE</h1>
-            </div>
-        </section>
-     );
+  return (
+    <section id="quote">
+      <div>
+        <h1 className="title">REQUEST A QUOTE</h1>
+      </div>
+    </section>
+
+
+  );
 }
+
  
 export default Quote;
